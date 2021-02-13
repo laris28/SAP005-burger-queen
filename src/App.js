3ec9bb5cfcb3a13e891259c2248d26b2f416615e
laris28 <larissalmeida2002@gmail.com>
2021-02-13 15:24:50 -0300

    test1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+// import User from './components/User';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <User/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, {useState, useEffect} from 'react';
+import User from './User';
+
+const App = () => {
+  const [users, setUsers] = userState(data);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(()=> {
+    fetch('https://pokeapi.co/api/v2/type/5')
+      .then(result => result.json())
+      .then(result => {
+        console.log(result)
+        setPokemons(result.pokemon)
+      });
+    }, []);
+
+  // useEffect(()=> {
+  //   console.log('Input description foi alterado!')
+  // }, [users]);
+
+  // useEffect(()=> {
+  //   console.log('Input name foi alterado!')
+  // }, [users]);
+
+  // useEffect(()=> {
+  //   console.log('Users foi alterado!')
+  // }, [users]);
+
+  // useEffect(()=> {
+  //   console.log('Algum estado foi alterado!')
+  // }, [users]);
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+
+  //   setUsers(...users, {name, description}]);
+  // }
+
+
+  {
+    pokemons.map((item, index) => {
+      return(
+        <p key= {index}>{item.pokemon.name}</p>
+      )
+    })
+  }
 }
 
 export default App;
---
+++ src/components/User.js
@@ -0,0 +1,10 @@
+const User = () => {
+  return (
+    <div>
+        <p>Laris</p>
+        <p>Gosta de chocolate</p>
+    </div>
+  );
+}
+
+export default User;
