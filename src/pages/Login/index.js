8aa1736d16158e769025d61a6450d79ff598c495
laris28 <larissalmeida2002@gmail.com>
2021-02-15 02:48:42 -0300

    css do login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -12,31 +12,30 @@ const Login =()  =>{
     }*/
 
     return (
-    <div className="Login">
-        <div className= "login-logo">
-            <img src="https://cdn3.iconfinder.com/data/icons/eldorado-stroke-food-2/40/burger_2-512.png" alt="Burger, hamburger, cheeseburger icon - Download on Iconfinder"/>
-       <h1>Login</h1>
-       <form>	
-      <label htmlFor="email" className="label">E-mail:</label>	
-      <br></br>
-      <input className="input" type="text" value={email} onChange={e => setEmail(e.target.valeu)} name="email"></input>	
-      <br></br>		
-      <label htmlFor="password" className="label">Senha:</label>	
-      <br></br>	
-      <input className="input" type={ show ? "text" : "password"}
-       value={password} onChange={e => setPassword(e.target.value)} name="password"></input>	
-      <br></br>
-      
-     {/* <div className="login-eye"> {show ? (<HiEye size={20} onClick={handleClick} />) : (<HiEyeOff size={20} onClick={handleClick} />)} </div></br>*/}	
+        <div className="Login">
+            
+                <form class="login">	
+                    <h1>BURGER QUEEN</h1>
+                    <label htmlFor="email" className="label">E-mail:</label>	
+                    <br></br>
+                    <input className="input" type="text" value={email} onChange={e => setEmail(e.target.valeu)} name="email"></input>	
+                    <br></br>		
+                    <label htmlFor="password" className="label">Senha:</label>	
+                    <br></br>	
+                    <input className="input" type={ show ? "text" : "password"}
+                    value={password} onChange={e => setPassword(e.target.value)} name="password"></input>	
+                    <br></br>
+        
+        {/* <div className="login-eye"> {show ? (<HiEye size={20} onClick={handleClick} />) : (<HiEyeOff size={20} onClick={handleClick} />)} </div></br>*/}	
 
-      <input type="submit" className="form-button" value="Entrar"></input>	
-      
-    </form>	
-    <p className="p-bottom">Ainda não possui conta? <a href="/Register">Cadastre-se</a></p>	
-             </div>
-             </div>
-      )
-      }
+                    <input type="submit" className="form-button" value="Entrar"></input>	
+                    <p className="p-bottom">Ainda não possui conta? <a href="/Register">Cadastre-se</a></p>	
+        
+                </form>	
+            
+        </div>
+    )
+}
 
 
 export default Login;
