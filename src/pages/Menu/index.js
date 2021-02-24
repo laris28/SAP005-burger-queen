6ca387383cb5c06c695027e5e1d524f953ef2e97
laris28 <larissalmeida2002@gmail.com>
2021-02-23 22:08:12 -0300

    produtos printados na tela

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -1,80 +1,121 @@
-import React, { useState } from 'react';
+import {useEffect, useState} from 'react'
+import { useHistory } from "react-router-dom";
+
 
 export const Menu = () => {
-    const [hamburgers, updateData] = useState ([]);
+  const [menu, setMenu] = useState([]);
+  const [client,setClient] = useState('');
+  const [table,setTable] = useState('');
+  const [send, setSend] = useState('');
 
-    let token = localStorage.getItem('token');
-    console.log(token);
-
-        const myHeader = new Headers();
-        myHeader.append('Authorization', token);
-
-        const [client,setClient] = useState('');
-        const [table,setTable] = useState('');
         
-        fetch('https://lab-api-bq.herokuapp.com/products', {
-            method: 'GET',
-            headers: myHeader,
-            body: JSON.stringify()
-        })
-        .then(response => response.json())
-        .then((response) => {
-            updateData(response);
-        }, [])
-        .then(response => console.log(response))
-        .catch(error => console.log('error', error));
+  // fetch('https://lab-api-bq.herokuapp.com/products', {
+  //   method: 'GET',
+  //   headers: myHeader,
+  //   body: JSON.stringify()
+  // })
+  // .then(response => response.json())
+  // .then((response) => {
+  //   updateData(response);
+  // }, [])
+  // .then(response => console.log(response))
+  // .catch(error => console.log('error', error));
 
-        var myHeaders = new Headers();
-myHeaders.append("Authorization", token);
-myHeaders.append("Content-Type", "application/json");
+  // var myHeaders = new Headers();
+  // myHeaders.append("Authorization", token);
+  // myHeaders.append("Content-Type", "application/json");
 
-var raw = JSON.stringify({"client":client,"table":table,"products":[{"id":29,"qtd":8}]});
+  var raw = {"client":client, "table":table, "products":[{"id":29, "qtd":8}]};
 
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
+  // var requestOptions = {
+  //   method: 'POST',
+  //   headers: myHeaders,
+  //   body: raw,
+  //   redirect: 'follow'
+  // };
 
-fetch("https://lab-api-bq.herokuapp.com/orders", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+  // fetch("https://lab-api-bq.herokuapp.com/orders", requestOptions)
+  //   .then(response => response.text())
+  //   .then(result => console.log(result))
+  //   .catch(error => console.log('error', error));
 
-  
+  const getProducts = () => {
+    const token = localStorage.getItem('token');
+    return fetch("https://lab-api-bq.herokuapp.com/products", {
+      method: 'GET',
+      headers: {Authorization: token},
+      redirect: 'follow'
+    })
     
-    return(
+  }
+  const postOrders = () => {
+    const token = localStorage.getItem('token');
+    fetch("https://lab-api-bq.herokuapp.com/orders", {
+      method: 'POST',
+      headers: {
+        "Authorization": token,
+        "Content-Type": "application/json"
+      },
+      
+      body: JSON.stringify(raw),
+      // redirect: 'follow'
+      
+    })
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+  }
+  useEffect(() => {
+    getProducts()
+    .then(response => response.json())
+    .then(result => setMenu(result))
+    .catch(error => console.log('error', error));
+  }, []);
 
+  return (
+    <>
+      <h1>Bem vindo ao menu</h1>
+      <br></br>
+      <h2>Café da Manhã</h2>
+      {menu.map(function(item){
+        console.log(item)
+        return(
+          <p key={item.id}>{item.name}</p>
+        )
+      })}
 
-        <div>
-            <input type = "text" placeholder = "Nome do cliente" value={client} onChange={e=> setClient(e.target.value)}/>
-         <input type = "text" placeholder = "Mesa" value={table} onChange={e=> setTable(e.target.value)}/>
-         <button className="form-button" type='submit' onClick={(e) => {
-                e.preventDefault();
-                
-                }}>Enviar Pedido</button>
-            <h1>Menu</h1>
-            {hamburgers.map(({id, name, price, image}) => {
-                return(
-                    <div className='container'>
-                        <div className='card'>
-                            <div class="card-container">
-                                <li key={id }>
-                                    <div className="hamburgers-thumb">
-                                        <img src={image} alt={`${name} Thumb`} />
-                                    </div>
-                                    <p>{ name }</p>
-                                    <p>R${ price }</p>
-                                    <button id='submitItem'>Adicionar</button>
-                                </li>
-                            </div>
-                        </div>
-                    </div>
-                );
-            })}
-        </div>
-    );
+      <input type = "text" placeholder = "Client" value={client} onChange={e=> setClient(e.target.value)}/>
+      <input type = "text" placeholder = "Mesa" value={table} onChange={e=> setTable(e.target.value)}/>
+      <button className="form-button" type='submit' onClick={(e) => {
+        e.preventDefault();
+        postOrders();
+      }}>Enviar Pedido</button>
+    </>
+  );
 }
+
+// {menu.map(({id, name, price, image}) => {
+//   return(
+//     <div>
+//       <input type = "text" placeholder = "Nome do cliente" value={client} onChange={e=> setClient(e.target.value)}/>
+//       <input type = "text" placeholder = "Mesa" value={table} onChange={e=> setTable(e.target.value)}/>
+//       <button className="form-button" type='submit' onClick={(e) => {
+//         e.preventDefault();
+//       }}>Enviar Pedido</button>
+//       <h1>Menu</h1>
+//       <div className='container'>
+//         <div className='card'>
+//           <div class="card-container">
+//             
+//           </div>
+//         </div>
+//       </div>
+
+//     </div>
+
+//   );
+
+// });
+// }
 
 export default Menu;
